808c9be33a383bdbfb86177c9d133c06a3ff01e9
Adnan Husain <adnanhusain95@gmail.com>
2016-10-15 11:58:40 +0530

    Update main.py
    Successfully able to find and draw contours (boundary) around any object in the Region of Interest.
    
    Crucial for Object Detection and Comparison.

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ while (cap.isOpened()):
 
     img1 = img[20:300,20:300]
 
+    imCopy = img1.copy()
+    
     gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    ret, thresh1 = cv2.threshold(blur, 200, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    ret, thresh1 = cv2.threshold(blur, 10, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
     cv2.imshow("Threshold", thresh1)
+
+    contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(imCopy, contours, -1, (0, 255, 0))
+    cv2.imshow('draw contours', imCopy)
+
 
     k = 0xFF & cv2.waitKey(10)  # escape kep value = 27
     if k == 27:
